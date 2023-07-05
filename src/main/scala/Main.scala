690f9ec148811b7b05272297aba60e6cd5c3c79b
Alexandre Ferreira <ferreiraalexandre468@gmail.com>
2023-07-04 23:21:01 -0300

    first commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+/*
+This is a file for main
+*/
+
+/**
+ * This is the main object for our first project.
+  */
+object HelloWorld:
+  /**
+   * This is the main method for the aplication.
+   * @
+   * */
+  def main(args: Array[String]):Unit=
+    println("hello world")
